1e3181fc2be03c8cc272226ce0e4b07759b6a34f
Arnold de Roij <arnold.de.rooij@asml.com>
2021-09-09 16:42:15 +0200

    split spec files

---
+++ customer.fast-check.spec.ts
@@ -0,0 +1,41 @@
+import * as fc from "fast-check";
+import {Customer} from "./customer";
+
+
+describe("Customer fast-check version", () => {
+  describe('isAllowedToBuyAlcohol', () => {
+    it('true better version', () => {
+      fc.assert(
+        fc.property(
+          fc.record({
+            name: fc.string(),
+            age: fc.integer({min: 18}),
+            hobbies: fc.array(fc.string()),
+          }),
+          (data) => {
+            const customer = new Customer(data.name, data.age, data.hobbies);
+            expect(customer.isAllowedToBuyAlcohol()).toEqual(true);
+          }
+        ),
+        {verbose: true, unbiased: true}
+      );
+    });
+
+    it('false better version', () => {
+      fc.assert(
+        fc.property(
+          fc.record({
+            name: fc.string(),
+            age: fc.integer({max: 17}),
+            hobbies: fc.array(fc.string()),
+          }),
+          (data) => {
+            const customer = new Customer(data.name, data.age, data.hobbies);
+            expect(customer.isAllowedToBuyAlcohol()).toEqual(false);
+          }
+        ),
+        {verbose: true, unbiased: true}
+      );
+    });
+  });
+});
