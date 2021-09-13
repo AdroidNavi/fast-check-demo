0044ad45061387acf0d7aa41dcf2af989f3b9a95
Arnold <ahrl.de.roij@gmail.com>
2021-09-13 20:08:25 +0200

    improve test readability

--- customer.fast-check.spec.ts
+++ customer.fast-check.spec.ts
@@ -1,44 +1,32 @@
 import * as fc from "fast-check";
+import { IntegerConstraints } from "fast-check";
 import { Customer } from "./customer";
 
-type CustomerPropertyData = {
-  name: string;
-  age: number;
-  hobbies: string[];
-}
+const createArbitrary = (ageConstrains: IntegerConstraints) =>
+  fc.record({
+    name: fc.string(),
+    age: fc.integer(ageConstrains),
+    hobbies: fc.array(fc.string()),
+  });
 
 describe("Customer (fast-check)", () => {
   describe("isAllowedToBuyAlcohol", () => {
     it("allowed", () => {
       fc.assert(
-        fc.property(
-          fc.record({
-            name: fc.string(),
-            age: fc.integer({ min: 18 }),
-            hobbies: fc.array(fc.string()),
-          }),
-          (data: CustomerPropertyData) => {
-            const customer = new Customer(data.name, data.age, data.hobbies);
-            expect(customer.isAllowedToBuyAlcohol()).toEqual(true);
-          }
-        ),
+        fc.property(createArbitrary({ min: 18 }), (data) => {
+          const customer = new Customer(data.name, data.age, data.hobbies);
+          expect(customer.isAllowedToBuyAlcohol()).toEqual(true);
+        }),
         { verbose: true, unbiased: true }
       );
     });
 
     it("not allowed", () => {
       fc.assert(
-        fc.property(
-          fc.record({
-            name: fc.string(),
-            age: fc.integer({ max: 17 }),
-            hobbies: fc.array(fc.string()),
-          }),
-          (data: CustomerPropertyData) => {
-            const customer = new Customer(data.name, data.age, data.hobbies);
-            expect(customer.isAllowedToBuyAlcohol()).toEqual(false);
-          }
-        ),
+        fc.property(createArbitrary({ max: 17 }), (data) => {
+          const customer = new Customer(data.name, data.age, data.hobbies);
+          expect(customer.isAllowedToBuyAlcohol()).toEqual(false);
+        }),
         { verbose: true, unbiased: true }
       );
     });
